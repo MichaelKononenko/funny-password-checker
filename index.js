59bcfa5b935e1621c5c4860cf584b3cd7656c30a
Michael <michael.kononenko101@gmail.com>
2022-10-18 10:58:40 +0300

    change the movement logic

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ const password = document.querySelector('input[type="password"]');
 const about = document.querySelector(".about");
 const backdrop = document.querySelector(".backdrop");
 
-submit.addEventListener("click", doMagic);
-
 let goto = 100;
 
-function doMagic(event) {
-  event.preventDefault();
+submit.addEventListener("mouseover", doMagic);
+password.addEventListener("input", () =>
+  password.value.length >= 8
+    ? (submit.style = `background: rgb(62, 242, 62);`)
+    : (submit.style = "background:null;")
+);
+
+function doMagic() {
   if (password.value.length >= 8) {
+    submit.addEventListener("click", clickHandling);
     return;
   } else {
+    submit.removeEventListener("click", submitClickHandling);
     submit.style = `transform:translateX(${goto}px); transition:transform linear 200ms;`;
     goto === 100 ? (goto = 0) : (goto = 100);
   }
+}
+
+function submitClickHandling(event) {
+  event.preventDefault();
+  console.log("hello");
 }
 
 about.addEventListener("click", () => backdrop.classList.remove("is-hidden"));
