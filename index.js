f9eea45d1d52a2ca3cbe7da3e66c15ccd9b58a7d
Michael <michael.kononenko101@gmail.com>
2022-10-20 00:26:03 +0300

    looks good

--- index.js
+++ index.js
@@ -2,30 +2,49 @@ const submit = document.querySelector("button");
 const password = document.querySelector('input[type="password"]');
 const about = document.querySelector(".about");
 const backdrop = document.querySelector(".backdrop");
+const redirectInfo = document.querySelector(".redirect-info");
+
+const displayWidth = document.documentElement.clientWidth;
 
 let goto = 100;
 
-submit.addEventListener("mouseover", doMagic);
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (password.value.length >= 8) {
+    redirectInfo.classList.toggle("is-hidden");
+    setTimeout(
+      () => (location.href = "https://github.com/MichaelKononenko/funny-password-checker"),
+      1500
+    );
+  }
+});
+
+displayWidth > 760
+  ? submit.addEventListener("mouseover", mouseHandling)
+  : submit.addEventListener("click", clickHandling);
+
 password.addEventListener("input", () =>
   password.value.length >= 8
-    ? (submit.style = `background: rgb(62, 242, 62);`)
-    : (submit.style = "background:null;")
+    ? (submit.style.backgroundColor = `rgb(62, 242, 62)`)
+    : (submit.style.backgroundColor = null)
 );
 
-function doMagic() {
+function mouseHandling(event) {
   if (password.value.length >= 8) {
-    submit.addEventListener("click", clickHandling);
     return;
   } else {
-    submit.removeEventListener("click", submitClickHandling);
     submit.style = `transform:translateX(${goto}px); transition:transform linear 200ms;`;
     goto === 100 ? (goto = 0) : (goto = 100);
   }
 }
 
-function submitClickHandling(event) {
-  event.preventDefault();
-  console.log("hello");
+function clickHandling(event) {
+  if (password.value.length >= 8) {
+    return;
+  } else {
+    submit.style = `transform:translateX(${goto}px); transition:transform linear 200ms;`;
+    goto === 100 ? (goto = 0) : (goto = 100);
+  }
 }
 
 about.addEventListener("click", () => backdrop.classList.remove("is-hidden"));
